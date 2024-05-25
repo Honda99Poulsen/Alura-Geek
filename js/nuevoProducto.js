5e7f57baa6968222b860e8651a0a7760298159b5
EliasZavala <eliaszavala1216@gmail.com>
2024-05-25 10:46:04 -0600

    Se agregan articulos nuevos

---
+++ js/nuevoProducto.js
@@ -0,0 +1,20 @@
+import { ConexionApi } from "./conexionAPI.js";
+
+
+const formulario = document.querySelector("[data-form]");
+
+async function crearNuevoProducto(){
+
+    const valorNombre = document.querySelector("[data-nombre]").value;
+    const valorPrecio = document.querySelector("[data-precio]").value;
+    const valorUrlImagen = document.querySelector("[data-imagen]").value;
+
+    await ConexionApi.crearArticuloProducto(valorNombre, valorPrecio, valorUrlImagen);
+
+}
+
+formulario.addEventListener("submit", ( evento ) => {
+    evento.preventDefault()
+    crearNuevoProducto()
+})
+
