140e376f25bba4545dcd44e0a3239f9b4c7809a7
EliasZavala <eliaszavala1216@gmail.com>
2024-05-30 17:55:15 -0600

    Se ha creado limpiarInput y limpia la caja

---
+++ js/services/limpiarInput.js
@@ -0,0 +1,12 @@
+
+const limpiar = document.querySelector("[data-limpiar]");
+
+limpiar.addEventListener("click", () => {
+
+    const campoNombre = document.querySelector("[data-nombre]").value = "";
+    const campoPrecio = document.querySelector("[data-precio]").value= "";
+    const campoUrl = document.querySelector("[data-imagen]").value = "";
+
+    console.log('Campos limpiados');
+
+})
