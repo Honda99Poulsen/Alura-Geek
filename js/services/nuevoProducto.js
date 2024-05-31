97b37749b29ac99ee301f42d0a6487af9116a65c
EliasZavala <eliaszavala1216@gmail.com>
2024-05-31 11:29:35 -0600

    Se ha eliminado verificacion de URL

--- js/services/nuevoProducto.js
+++ js/services/nuevoProducto.js
@@ -9,27 +9,11 @@ async function crearNuevoProducto(evento){
     const valorPrecio = document.querySelector("[data-precio]").value;
     const valorUrlImagen = document.querySelector("[data-imagen]").value;
 
-    const urlPattern = new RegExp('^(https?:\\/\\/)?' + 
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + 
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + 
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + 
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + 
-        '(\\#[-a-z\\d_]*)?$', 'i'); 
-
-        if(urlPattern.test(valorUrlImagen)) {
-            alert('URL valida');
-        }else{
-            alert('URL invalida');
-        }
-
-
     await ConexionApi.crearArticuloProducto(valorNombre, valorPrecio, valorUrlImagen);
 
 }
 
 formulario.addEventListener("submit", ( evento ) => {
-    evento.preventDefault()
-
         crearNuevoProducto()
     
 })
